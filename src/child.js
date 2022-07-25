a95a6607d7b3a2e7a9c133a7694ef1abda94bacd
unknown <ycygh@naver.com>
2022-07-25 17:20:45 +0900

    project init

---
+++ src/child.js
@@ -0,0 +1,13 @@
+function Childcomponent(props) {
+  const { name, age } = props;
+
+  return (
+    <div>
+      <p>
+        이름은 {name}이며 나이는 {age}입니다.
+      </p>
+    </div>
+  );
+}
+
+export default Childcomponent;
